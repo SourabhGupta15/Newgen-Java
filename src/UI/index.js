96289dc82d65ff23aa15a30caafae8a47f9131d6
SourabhGupta15 <guptasourabh500@gmail.com>
2025-05-15 21:45:44 +0530

    Add Typescript

---
+++ src/UI/index.js
@@ -0,0 +1,19 @@
+function greet(name) {
+    return "Hello, ".concat(name, "!");
+}
+console.log(greet("Sourabh"));
+var num = 12;
+var str = "abc";
+var boo = true;
+var num1 = 12;
+var str1 = "abc";
+console.log(greet(str));
+console.log(greet(str1));
+console.log(boo);
+console.log(num);
+var v = true;
+console.log(v);
+v = "xyz";
+console.log(v);
+v = 156;
+console.log(v);
---
+++ src/UI/index.ts
@@ -0,0 +1,26 @@
+function greet(name: string): string{
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("Sourabh"));
+
+let num: number = 12;
+let str: string = "abc";
+let boo: boolean = true;
+
+let num1 = 12;
+let str1 = "abc";
+
+console.log(greet(str));
+console.log(greet(str1));
+console.log(boo);
+console.log(num);
+
+let v: any = true;
+console.log(v);
+
+v = "xyz";
+console.log(v);
+
+v = 156;
+console.log(v);
