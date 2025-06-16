ff19298cfe97b8d1d12a20affbabc883f76516c9
SourabhGupta15 <guptasourabh500@gmail.com>
2025-06-16 23:11:02 +0530

    Within Component Binding

--- src/UI/angular/my-first-app/src/app/home/home.ts
+++ src/UI/angular/my-first-app/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
+  headline = 'message from Home';   //Normal Variable
+  signalMessage = signal('Signal Message from HomeComponent');    //Signal Variable
 
+  message = signal('Another Message');
+  changeMessage() {
+    console.log("changeMessage called");
+    this.message.update(() => 'You clicked the button');
+  }
 }
