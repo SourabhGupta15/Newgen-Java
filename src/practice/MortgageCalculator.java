d0a3daf373c3637159e4091268ac96ab7e9402af
SourabhGupta15 <guptasourabh500@gmail.com>
2025-07-03 04:13:01 +0530

    Added changes to optimize MortgageCalculator

--- src/practice/MortgageCalculator.java
+++ src/practice/MortgageCalculator.java
@@ -1,37 +1,84 @@
 package practice;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MortgageCalculator {
     final static byte MONTHS_IN_YEAR = 12;
     final static byte PERCENT = 100;
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int principal = (int) readNumber("Principal ($1K - $1M)", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate", 1, 30);
+        byte years = (byte) readNumber("Period (Years)", 1, 30);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
-
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double mortgage = mortgageCalculator(principal, monthlyInterest, numberOfPayments);
-        
-        System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
-
-        scanner.close();
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
     }
 
-    public static double mortgageCalculator(int principal, float monthlyInterest, int numberOfPayments) {
-        
+    private static void printMortgage(int principal, float annualInterest, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + NumberFormat.getCurrencyInstance(Locale.US).format(mortgage));
+    }
+
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format(balance));
+        }
+    }
+
+    public static double readNumber(String prompt, double min, double max) {
+        double value = 0;
+
+        while (true) {
+            System.out.print(prompt + ": ");
+            value = scanner.nextDouble();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+
+        return value;
+    }
+
+    public static double calculateBalance(
+            int principal,
+            float annualInterest,
+            byte years,
+            short numberOfPaymentsMade) {
+
+        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        double balance = principal
+                * (Math.pow((1 + monthlyInterest), numberOfPayments) - Math.pow((1 + monthlyInterest), numberOfPaymentsMade))
+                / (Math.pow((1 + monthlyInterest), numberOfPayments) - 1);
+
+        return balance;
+    }
+
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years) {
+
+        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
         double val = Math.pow((1 + monthlyInterest), (numberOfPayments));
+
         double mortgage = (principal * monthlyInterest * val) / (val - 1);
+
         return mortgage;
     }
 }
